64afecf05587fc7d1c845bdb8b9d146ce8c54e45
Nitin Issac Joy <nitin.i.joy@gmail.com>
2019-01-13 11:59:37 +0530

    Building Timeline for a year

--- App.js
+++ App.js
@@ -12,7 +12,19 @@ export default class App extends React.Component {
         <MapView style={StyleSheet.absoluteFillObject} />
         <ScrollView horizontal={true} style={{ position: 'absolute', bottom: 0, left: 0, right: 0, height: 150, width:'100%', borderWidth: 1 }}>
             
-          <TimelineElement />
+          <TimelineElement  month="2019"/>
+          <TimelineElement  month="Jan"/>
+          <TimelineElement  month="Feb"/>
+          <TimelineElement  month="Mar"/>
+          <TimelineElement  month="Apr"/>
+          <TimelineElement  month="May"/>
+          <TimelineElement  month="Jun"/>
+          <TimelineElement  month="Jul"/>
+          <TimelineElement  month="Aug"/>
+          <TimelineElement  month="Sep"/>
+          <TimelineElement  month="Oct"/>
+          <TimelineElement  month="Nov"/>
+          <TimelineElement  month="Dev"/>
         </ScrollView>
       </View>
     );
--- TimelineElement.js
+++ TimelineElement.js
@@ -4,28 +4,38 @@ import { Text, ScrollView, StyleSheet, View } from 'react-native';
 
 const styles = StyleSheet.create({
     el: {
-        width: 100,
+        position: 'absolute',
+        width: 60,
         height: 100,
         top: 8,
-        borderTopWidth: 2,
+        borderTopWidth: 5,
         borderColor: 'grey'
     },
     point: {
-        width: 100,
-        height: 100,
-        top: 2,
-        borderTopWidth: 2,
-        borderColor: 'blue'
+        position: 'absolute',
+        top: 5,
+        left: 20,
+        borderRadius: 6,
+        borderWidth: 6,
+        borderColor: 'white'
     }
 });
 
 export default class TimelineElement extends React.Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            month: this.props.month
+        }
+    }
+
     render() {
         return (
-            <View>
+            <View style={{position: 'relative' }}>
                 <View style={styles.el} />
                 <View style={styles.point} />
+                <Text style={{left: 0, top: 25}}> {this.state.month} </Text>
             </View>
         );
     }

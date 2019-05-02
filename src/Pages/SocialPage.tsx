a5e5eadf6a83dae10acc768fd9e41b62d834357f
Nitin Issac Joy <nitin.i.joy@gmail.com>
2019-05-02 19:05:07 +0530

    Showing live location for users

--- src/Pages/SocialPage.tsx
+++ src/Pages/SocialPage.tsx
@@ -1,11 +1,13 @@
 import * as React from 'react';
 import { View, StyleSheet } from 'react-native';
-import MapView, { Callout } from 'react-native-maps';
+import MapView, { Callout, Marker } from 'react-native-maps';
 import { SliderItems } from '../Modals/ApplicationEnums';
+import Region from '../Modals/Region';
 import SnapSlider from '../UIComponents/SnapSlider';
 
 interface IState {
-
+    markers: Array<any>,
+    region: Region
 }
 
 interface IProps {
@@ -18,6 +20,10 @@ export default class SocialPage extends React.Component<IProps, IState> {
     constructor(props: IProps) {
         super(props);
 
+        this.state = {
+            markers: [],
+            region: new Region(0, 0, 0, 0)
+        }
         fetch('http://localhost:8080/location/v1/nearme', {
             method: 'POST',    
             headers: {
@@ -32,14 +38,49 @@ export default class SocialPage extends React.Component<IProps, IState> {
         })
         .then((response) => response.json())
         .then((response) => {
-            console.log(response);
+            if(response.profileArray == undefined) return;
+            this.setState({
+                markers: response.profileArray,
+                region: this.calculateRegion(response.profileArray)
+            });
         })
+    }
+
+    calculateRegion(markers: Array<any>) : Region {
+
+
+        var latitudeSum: number = 0;
+        var longitudeSum: number = 0;
+
+        for(var i = 0; i < markers.length; i++) {
+            latitudeSum += markers[i].latitude;
+            longitudeSum += markers[i].longitude;
+        }
+
+        return new Region(latitudeSum/markers.length, longitudeSum/markers.length, 0, 0);
     }
 
     render() {
         return(
             <View style={StyleSheet.absoluteFillObject}>
-                <MapView style={StyleSheet.absoluteFillObject}>
+                <MapView style={StyleSheet.absoluteFillObject} region={this.state.region}>
+                    {
+                        console.log(this.state.markers)
+                    }
+                    {
+                        this.state.markers.map((marker, index) => (
+                            <Marker
+                            key={marker.longitude}
+                            coordinate={marker}
+                            >
+                            <View style={{
+                                width: 30,
+                                height: 30,
+                                borderWidth: 1}}>
+                            </View>
+                            </Marker>
+                        ))
+                    }
                     <Callout style={{ top: 50, left: 120, width: 140, height: 50, borderWidth: 1}}> 
                         <SnapSlider 
                             style={{ top: 50, left: 120}}

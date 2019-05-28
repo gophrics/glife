9746ed569b8cf15bc4f9151c85a5bb0e3baa0ae7
Nitin Issac Joy <nitin.i.joy@gmail.com>
2019-05-29 01:01:46 +0530

    Oh yeah throwing away stupid stuff

--- android/app/src/main/java/com/glife/MainApplication.java
+++ android/app/src/main/java/com/glife/MainApplication.java
@@ -3,7 +3,6 @@ package com.glife;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.imagepicker.ImagePickerPackage;
 import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
 import com.airbnb.android.react.maps.MapsPackage;
 import com.facebook.react.ReactNativeHost;
@@ -29,7 +28,6 @@ public class MainApplication extends Application implements ReactApplication {
       return Arrays.<ReactPackage>asList(
             new SQLitePluginPackage(),   // register SQLite Plugin here
             new MainReactPackage(),
-            new ImagePickerPackage(),
             new AsyncStoragePackage(),
             new MapsPackage(),
       	    new SvgPackage()

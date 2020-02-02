9a95aaa553ff187b55d5b20851695eeb2f7e0563
Nitin Issac Joy <nitin.i.joy@gmail.com>
2020-02-02 23:05:31 +0530

    Unit test with PowerMockito and Robolectric getting up

--- android/app/src/test/java/com/beerwithai/glimpse/UnitTest/EngineTests.java
+++ android/app/src/test/java/com/beerwithai/glimpse/UnitTest/EngineTests.java
@@ -1,53 +1,76 @@
+/*
+ * Copyright 2015 Realm Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.beerwithai.glimpse.UnitTest;
 
-import android.content.Context;
-
+import com.beerwithai.glimpse.BuildConfig;
 import com.beerwithai.glimpse.Engine.Engine;
 import com.facebook.react.bridge.ReactApplicationContext;
-import org.junit.After;
+
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.junit.MockitoJUnit;
-import org.mockito.junit.MockitoRule;
-
-import java.io.File;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PowerMockIgnore;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.core.classloader.annotations.SuppressStaticInitializationFor;
+import org.powermock.modules.junit4.rule.PowerMockRule;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
 
 import io.realm.Realm;
+import io.realm.log.RealmLog;
 
-import static org.mockito.Mockito.when;
+import static org.powermock.api.mockito.PowerMockito.mockStatic;
+import static org.powermock.api.mockito.PowerMockito.when;
 
 
+@RunWith(RobolectricTestRunner.class)
+@PowerMockIgnore({"org.mockito.*", "org.robolectric.*", "android.*"})
+@SuppressStaticInitializationFor("io.realm.internal.Util")
+@PrepareForTest({Realm.class, RealmLog.class})
 public class EngineTests {
+    // Robolectric, Using Power Mock https://github.com/robolectric/robolectric/wiki/Using-PowerMock
+
+    @Rule
+    public PowerMockRule rule = new PowerMockRule();
+    Realm mockRealm;
 
     @Mock
     ReactApplicationContext context;
 
-    @Rule
-    public MockitoRule rule = MockitoJUnit.rule();
-
     @Before
     public void setup() {
-        Context c = Mockito.mock(Context.class);
-        when(c.getFilesDir()).thenReturn(new File("/"));
-        Realm.init(c);
+        mockStatic(RealmLog.class);
+        mockStatic(Realm.class);
+
+        Realm mockRealm = PowerMockito.mock(Realm.class);
+
+        when(Realm.getDefaultInstance()).thenReturn(mockRealm);
+
+        this.mockRealm = mockRealm;
     }
 
     @Test
-    public void RunEngine() {
-        try {
-            Engine Instance = new Engine();
-            Instance.Initialize(context);
-            assert (true);
-        } catch (Exception e) {
-            assert (false);
-        }
+    public void Engine_Initialization_Test() {
+        Engine e = new Engine();
+        e.Initialize(context);
     }
 
-    @After
-    public void bye() {
-
-    }
 }
